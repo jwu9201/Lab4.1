699efc686b7aba49f86cebb9a9b39cb2e267af78
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2018-03-16 09:28:19 -0400

    asdf

--- src/Deck.java
+++ src/Deck.java
@@ -12,6 +12,7 @@ public class Deck {
 		}
 	}
 	
+	
 	public boolean isEmpty() {
 		if (unDealt.size() == 0) {
 			return true;
@@ -34,8 +35,11 @@ public class Deck {
 			return deal;
 		}
 	}
-	public void Swap(Card card,int pos) {
-		unDealt.
+	public void Swap(int pos1,int pos2) {
+		Card card = unDealt.get(pos1);
+		unDealt.set(pos1, unDealt.get(pos2));
+		unDealt.set(pos2, card);
+		
 	}
 	
 	public void Shuffle() {
@@ -45,7 +49,18 @@ public class Deck {
 		}
 		for (int k = 51; k >= 0; k--) {
 			int r = (int)Math.random()*51;
-			
+			Swap(r,k);
+		}
+		for (int k = 51; k >= 0; k--) {
+			int pos = k;
+			for (int j = k; j >= 0; j--) {
+				Card select = unDealt.get(pos);
+				if (select.getValue() > unDealt.get(j).getValue()) {
+					select = unDealt.get(j);
+					pos = j;
+				}
+			}
+			Swap(pos,k);
 		}
 	}
 }
---
+++ src/DeckTester.java
@@ -0,0 +1,18 @@
+
+public class DeckTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] rank = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+		String[] suit = {"Diamonds", "Clubs", "Hearts", "Spades"};
+		int[] value = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+		
+		Deck test = new Deck(rank, suit, value);
+		
+		test.Shuffle();
+		test.Deal();
+		
+	}
+
+}
